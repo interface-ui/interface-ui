83c272ead5968c2b16a2b18825f97f296cf16ac5
Jacob Johnson <43164478+tsinghua-lau@users.noreply.github.com>
2023-08-12 23:26:16 +0800

    feat: switch component

---
+++ packages/fusion-ui/components/switch/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '../install'
+import Swtich from './src/index.vue'
+
+export const FnSwtich = withInstall(Swtich, 'FnSwtich')
+FnSwtich.name = 'FnSwtich'
+export default FnSwtich
---
+++ packages/fusion-ui/components/switch/src/index.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { UPDATE_MODEL_EVENT } from '../../../constans/event'
+import { useSwitch, useSwitchInner } from '../../_hooks'
+import { switchEmits, switchProps } from '../src/switch'
+
+const props = defineProps(switchProps)
+const emit = defineEmits(switchEmits)
+const { styleList, classList } = useSwitchInner(props)
+const { fnClassList } = useSwitch(props)
+const handleChange = (event: MouseEvent) => {
+  if (props.disabled)
+    return
+  emit(UPDATE_MODEL_EVENT, !props.modelValue as any)
+}
+</script>
+
+<template>
+  <div
+    :class="fnClassList"
+    :style="styleList"
+    @click="handleChange"
+  >
+    <div class="fn-switch__inner">
+      <!-- text -->
+      <div v-show="props.checkedText || props.unCheckedText" class="fn-switch__core">
+        <span v-show="props.checkedText && props.modelValue">
+          {{ props.checkedText }}
+        </span>
+        <span v-show="props.unCheckedText && !props.modelValue">
+          {{ props.unCheckedText }}
+        </span>
+      </div>
+
+      <!-- icon -->
+      <div v-show="props.checkedIcon || props.unCheckedIcon" class="fn-switch__core">
+        <span v-show="props.modelValue">
+          <fn-icon :icon="props.checkedIcon" :color="props.checkedIconColor" size="13" />
+        </span>
+        <span v-show="!props.modelValue">
+          <fn-icon :icon="props.unCheckedIcon" :color="props.unCheckedIconColor" size="13" />
+        </span>
+      </div>
+      <div class="fn-switch__button" :class="classList" />
+    </div>
+  </div>
+</template>
