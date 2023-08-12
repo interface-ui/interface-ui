efe6003d365cc0a0f50d43978260c4100bc3ee7e
Jacob Johnson <43164478+tsinghua-lau@users.noreply.github.com>
2023-08-12 23:20:58 +0800

    feat: index

--- packages/fusion-ui/components/_hooks/index.ts
+++ packages/fusion-ui/components/_hooks/index.ts
@@ -1,1 +1,4 @@
 export { useRadio, useRadioGroup } from './use-radio'
+export { useSwitchInner, useSwitch } from './use-switch'
+export { UseBadge } from './use-badge'
+export { UseProgress } from './use-progress'
