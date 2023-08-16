af3952f89927579941bbbe6ceb261b491f1bd907
Li_Sijie <19321115@hdu.edu.cn>
2023-08-16 11:59:18 +0800

    style: dialog与message过渡动画

--- packages/fusion-ui/components/dialog/src/index.vue
+++ packages/fusion-ui/components/dialog/src/index.vue
@@ -33,7 +33,8 @@ function handleClick(event: MouseEvent) {
 
 <template>
   <Teleport to="body">
-    <div
+    <transition name='dialog-fade'>
+      <div
       v-show="visible"
       ref="containerRef"
       class="dialog"
@@ -58,5 +59,8 @@ function handleClick(event: MouseEvent) {
         </div>
       </div>
     </div>
+    </transition>
+    
   </Teleport>
 </template>
+
