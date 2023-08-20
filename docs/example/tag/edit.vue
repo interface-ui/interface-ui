ebcc730b48dd5b9d46188cc2a6cdddb5997b1a04
Li_Sijie <19321115@hdu.edu.cn>
2023-08-20 12:39:13 +0800

    docs: edit Tag

---
+++ docs/example/tag/edit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div space-y-2>
+    <div fscw gap-2>
+      <fn-tag
+        v-for="tag in dynamicTags"
+        :key="tag"
+        closable
+        @close="handleClose(tag)"
+      >
+        {{ tag }}
+      </fn-tag>
+      <fn-button size="small" @click="addTag"> + New Tag </fn-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { nextTick, ref } from "vue"
+
+let inputValue = 4;
+const dynamicTags = ref(["Tag 1", "Tag 2", "Tag 3"]);
+
+const handleClose = (tag: string) => {
+  dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1);
+}
+
+const addTag = (tag:string)=>{
+  dynamicTags.value.push(`Tag${inputValue}`);
+  inputValue++;
+}
+</script>
+
