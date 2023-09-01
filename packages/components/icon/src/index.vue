6e1aceeb0f3a4fe470aeebbd231934ee75134310
Jacob Johnson <43164478+tsinghua-lau@users.noreply.github.com>
2023-09-02 00:34:44 +0800

    fix: try to fix the error report

--- packages/components/icon/src/index.vue
+++ packages/components/icon/src/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import { Icon as iconCom } from 'fusion-ui-iconify/dist/dist.mjs'
 import { iconProps } from './icon'
 const { icon, color, size } = defineProps(iconProps)
 </script>
 
 <template>
-  <icon :icon="icon" :color="color" :size="size" />
+  <icon-com :icon="icon" :color="color" :size="size" />
 </template>
 
 <style>
