45b661d32dc47148e62ebf445c0aabfed7c8d07e
sxiong5 <sxiong5@users.noreply.github.com>
2023-09-09 18:00:09 +0000

    deploy: e7d7adcc090ff71f220544e1712d40651f296ef6

---
+++ assets/langs_zh_guide_quick-start.md.80baa701.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as t,Q as i}from"./chunks/framework.40feac1c.js";const f=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"langs/zh/guide/quick-start.md"}'),r={name:"langs/zh/guide/quick-start.md"},s=i("",18),o=[s];function l(u,n,h,c,p,d){return t(),e("div",null,o)}const g=a(r,[["render",l]]);export{f as __pageData,g as default};
