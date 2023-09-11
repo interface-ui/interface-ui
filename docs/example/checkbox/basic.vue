b326c2aa250e240f7bd9259972552d2fc83dd77d
ShihaoXiong <shihao.xiong@icloud.com>
2023-09-11 14:58:00 -0400

    docs: add description for checkbox

--- docs/example/checkbox/basic.vue
+++ docs/example/checkbox/basic.vue
@@ -1,13 +1,19 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const checked = ref<boolean>(false)
+const checked1 = ref<boolean>(true)
+const checked2 = ref<boolean>(false)
+const checked3 = ref<boolean>(true)
+const checked4 = ref<boolean>(false)
 </script>
 
 <template>
   <div space-y-2>
     <div fscw gap-2>
-      <fn-checkbox v-model="checked" />
+      <fn-checkbox v-model="checked1" />
+      <fn-checkbox v-model="checked2" />
+      <fn-checkbox v-model="checked3" disabled />
+      <fn-checkbox v-model="checked4" disabled />
     </div>
   </div>
 </template>
