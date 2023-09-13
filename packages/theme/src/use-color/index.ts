9e6f981e9dfd685bf406a9626cb465e092c0590c
ShihaoXiong <shihao.xiong@icloud.com>
2023-09-13 16:07:43 -0400

    refactor: redefine elevation

--- packages/theme/src/use-color/index.ts
+++ packages/theme/src/use-color/index.ts
@@ -6,11 +6,11 @@ import useTheme from '@fusion-ui/theme/src/use-theme'
 import { computed } from 'vue'
 
 /**
- * The funtion to compute color from props
+ * The function to compute color from props
  * The props.color can be "primary" | "secondary" | "tertiary" | "error" | (theme: Theme) => string | "string"
  * Use this function to get the final color value
  * @param {ThemePaletteColor | string | ThemeCallBack} color
- * @param {string} [defaultVal] The default value when color is not exist
+ * @param {string} [defaultVal] The default value when color does not exist
  * @return Return the computed string. The value can be CSS variable or color in hex
  */
 const useColor = (
