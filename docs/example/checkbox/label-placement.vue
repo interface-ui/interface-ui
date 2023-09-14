6bfd13e3e391a9839e7f4318213904b8930fc319
ShihaoXiong <shihao.xiong@icloud.com>
2023-09-13 23:22:29 -0400

    docs: add label to checkbox doc

---
+++ docs/example/checkbox/label-placement.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const checked = ref<boolean>(true)
+</script>
+
+<template>
+  <div space-y-2>
+    <div fscw gap-2>
+      <FnFormLabel label="Top" label-placement="top">
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+      <FnFormLabel label="Left" label-placement="left">
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+      <FnFormLabel label="Bottom" label-placement="bottom">
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+      <FnFormLabel label="Right">
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+    </div>
+  </div>
+</template>
---
+++ docs/example/checkbox/label.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const checked = ref<boolean>(true)
+</script>
+
+<template>
+  <div space-y-2>
+    <div fscw gap-2>
+      <FnFormLabel label="Label">
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+      <FnFormLabel label="Required" required>
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+      <FnFormLabel label="Disabled" disabled>
+        <fn-checkbox v-model="checked" />
+      </FnFormLabel>
+    </div>
+  </div>
+</template>
