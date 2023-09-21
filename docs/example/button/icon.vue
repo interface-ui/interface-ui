b40091024a273a5b8d059803f27df03afe58479f
ShihaoXiong <shihao.xiong@icloud.com>
2023-09-21 14:19:57 -0400

    perf: scoped slot

--- docs/example/button/icon.vue
+++ docs/example/button/icon.vue
@@ -1,17 +1,13 @@
 <template>
   <div space-y-2>
     <div fscw gap-2>
-      <fn-button variant="outlined">
-        <template #default="icon">
-          <fn-icon icon="mdi:delete" v-bind="icon" />
-          delete
-        </template>
+      <fn-button variant="outlined" #default="icon">
+        <fn-icon icon="mdi:delete" v-bind="icon" />
+        delete
       </fn-button>
-      <fn-button>
-        <template #default="icon">
-          save
-          <fn-icon icon="mdi:content-save" v-bind="icon" />
-        </template>
+      <fn-button #default="icon">
+        save
+        <fn-icon icon="mdi:content-save" v-bind="icon" />
       </fn-button>
     </div>
   </div>
