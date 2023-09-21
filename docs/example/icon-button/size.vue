9602978f6aa9db6c4ba0b988febc5b7027c6eebb
ShihaoXiong <shihao.xiong@icloud.com>
2023-09-21 14:13:51 -0400

    docs: icon button for en doc

---
+++ docs/example/icon-button/size.vue
@@ -0,0 +1,15 @@
+<template>
+  <div space-y-2>
+    <div fscw gap-2>
+      <fn-icon-button size="small" #default="icon">
+        <fn-icon icon="mdi:delete" v-bind="icon" />
+      </fn-icon-button>
+      <fn-icon-button #default="icon">
+        <fn-icon icon="mdi:delete" v-bind="icon" />
+      </fn-icon-button>
+      <fn-icon-button size="large" #default="icon">
+        <fn-icon icon="mdi:delete" v-bind="icon" />
+      </fn-icon-button>
+    </div>
+  </div>
+</template>
