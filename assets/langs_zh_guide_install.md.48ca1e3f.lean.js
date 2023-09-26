289176cf11133c84205b55619adee7555514511e
tsinghua-lau <tsinghua-lau@users.noreply.github.com>
2023-09-26 12:12:38 +0000

    deploy: 1cf0aa665328bdd2bbd195041cdc7079a193b44f

---
+++ assets/langs_zh_guide_install.md.48ca1e3f.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,Q as p}from"./chunks/framework.41b990db.js";const B=JSON.parse('{"title":"安装使用","description":"","frontmatter":{},"headers":[],"relativePath":"langs/zh/guide/install.md"}'),l={name:"langs/zh/guide/install.md"},o=p("",19),e=[o];function c(t,r,y,i,A,D){return a(),n("div",null,e)}const d=s(l,[["render",c]]);export{B as __pageData,d as default};
--- assets/langs_zh_guide_quick-start.md.84efd025.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as t,Q as i}from"./chunks/framework.bd9bd2f3.js";const f=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"langs/zh/guide/quick-start.md"}'),r={name:"langs/zh/guide/quick-start.md"},s=i("",18),o=[s];function l(u,n,h,c,p,d){return t(),e("div",null,o)}const g=a(r,[["render",l]]);export{f as __pageData,g as default};
