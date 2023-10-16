b3692497ac69865a590c1533e92b0f07858ff40a
ShihaoXiong <shihao.xiong@icloud.com>
2023-10-16 19:16:04 -0400

    test: add more test for styled function

--- playground/src/Child.vue
+++ playground/src/Child.vue
@@ -2,13 +2,20 @@
 import { FnButton } from '@fusion-ui-vue/components'
 import { styled } from '@fusion-ui-vue/theme'
 
-const StyledButton = styled(FnButton)(theme => ({
+const StyledButton1 = styled(FnButton)(theme => ({
   '--fn-button-color': theme.palette.error,
 }))
+const StyledButton2 = styled(FnButton)`
+  background-color: red;
+  &:hover {
+    background-color: deeppink !important;
+  }
+`
 </script>
 
 <template>
   <div>
-    <StyledButton>button</StyledButton>
+    <StyledButton1>button</StyledButton1>
+    <StyledButton2>button</StyledButton2>
   </div>
 </template>
