3f384019eab5c4c627f035d761b9e2e0ce4e9309
ShihaoXiong <shihao.xiong@icloud.com>
2023-10-18 15:13:37 -0400

    perf: optimize styled function

--- packages/theme/src/core/styled.ts
+++ packages/theme/src/core/styled.ts
@@ -1,20 +1,28 @@
 import type { Component } from 'vue'
-import { computed, h } from 'vue'
+import { computed, defineComponent, h } from 'vue'
 import type { CSSInterpolation } from '@emotion/css'
 import { css } from '@emotion/css'
 import { useTheme } from '../hooks'
 import type { Theme } from './types'
 
-export const styled = (comp: Component) => {
+export const styled = (comp: Component | string) => {
   return (
-    style: TemplateStringsArray | ((theme: Theme) => CSSInterpolation)
+    style: TemplateStringsArray | ((theme: Theme) => CSSInterpolation),
+    ...args: CSSInterpolation[]
   ) => {
     const theme = useTheme()
-    return computed(() => {
-      const className =
-        style instanceof Function ? css(style(theme?.value)) : css(style)
 
-      return h(comp, { class: className })
+    return defineComponent({
+      setup(_, { slots }) {
+        const className = computed(() =>
+          style instanceof Function
+            ? css(style(theme?.value))
+            : css(style, ...args)
+        )
+
+        const slotsVNodes = slots?.default?.()
+        return () => h(comp as any, { class: className.value }, slotsVNodes)
+      },
     })
   }
 }
