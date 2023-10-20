4fde5135f09b671af338d35e1273d2766ac6a0e7
ShihaoXiong <shihao.xiong@icloud.com>
2023-10-20 14:47:39 -0400

    perf: ripple root style

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { FnButton, FnBadge, FnAvatar } from '@fusion-ui-vue/components'
+import { FnButton } from '@fusion-ui-vue/components'
 import createTheme from '@fusion-ui-vue/theme'
-import { ref } from 'vue'
-import Child from './Child.vue'
+import { toRaw } from 'vue'
 
-// const checked = ref<boolean>(false)
-// const picked = ref<string>('apple')
-const aaa = ref<number>(1)
-// const size = ref<number>(50)
 const theme = createTheme()
-console.log(theme.value)
+
+console.log(
+  `%c [Fusion UI] Tip: you can access the documentation 'theme' object directly in the console.`,
+  'color: #3894ff; font-weight: bold; font-size: 14px;'
+)
+;(window as any).theme = toRaw(theme.value)
 
 const changTheme = () => {
   theme.value.mode = theme.value.mode === 'dark' ? 'light' : 'dark'
@@ -17,67 +17,11 @@ const changTheme = () => {
 </script>
 
 <template>
-  <!-- -------------------浅色/深色模式切换------------------- -->
+  <!-- ------------------- Toggle between the light/dark mode ------------------- -->
   <header>
     <fn-button @click="changTheme">
       <!-- @vue-skip -->
       {{ theme.mode }}
     </fn-button>
-    <fn-button @click="aaa += 1"> Add size </fn-button>
   </header>
-
-  <!-- -------------------新组建开发调试------------------- -->
-  <div class="content">
-    <fn-badge content="1000" max="999">
-      <fn-button>Button</fn-button>
-    </fn-badge>
-    <fn-badge variant="dot" :color="theme => theme.colors.cyan[400]">
-      <fn-button>Button</fn-button>
-    </fn-badge>
-  </div>
-  <div class="content">
-    <Child :aaa="aaa" />
-  </div>
-  <div class="content">
-    <fn-avatar :background-color="theme => theme.colors.cyan[400]">
-      B
-    </fn-avatar>
-  </div>
-  <!-- <div class="content">
-    {{ size }}
-    <fn-avatar-group max="4" :size="size">
-      <fn-avatar
-        alt="Remy Sharp"
-        src="https://mui.com/static/images/avatar/1.jpg"
-      />
-      <fn-avatar
-        alt="Remy Sharp"
-        src="https://mui.com/static/images/avatar/2.jpg"
-      />
-      <fn-avatar
-        alt="Remy Sharp"
-        src="https://mui.com/static/images/avatar/3.jpg"
-      />
-      <fn-avatar
-        alt="Remy Sharp"
-        src="https://mui.com/static/images/avatar/4.jpg"
-      />
-      <fn-avatar
-        alt="Remy Sharp"
-        src="https://mui.com/static/images/avatar/5.jpg"
-      />
-    </fn-avatar-group>
-  </div>
-  <div>
-    <fn-button-group>
-      <fn-button>one</fn-button>
-      <fn-button>two</fn-button>
-      <fn-button>three</fn-button>
-    </fn-button-group>
-    <fn-button-group disabled>
-      <fn-button>one</fn-button>
-      <fn-button>two</fn-button>
-      <fn-button>three</fn-button>
-    </fn-button-group>
-  </div> -->
 </template>
