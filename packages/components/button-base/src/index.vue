93cc41e8c0a310be05ef68d636acefbf01e3ada1
ShihaoXiong <shihao.xiong@icloud.com>
2023-11-05 14:55:25 -0500

    style: button-base

--- packages/components/button-base/src/index.vue
+++ packages/components/button-base/src/index.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
 import { useNamespace } from '@fusion-ui-vue/utils'
+import { css } from '@fusion-ui-vue/theme'
 import { buttonBaseProps } from './button-base'
-import useCss from './index.jss'
 
 const props = defineProps(buttonBaseProps)
 const ns = useNamespace('button-base')
-const cssClass = useCss(props)
+const csStyle = props.cs ? css(props.cs) : ''
 </script>
 
 <template>
-  <component :is="$props.component" :class="[ns.b(), cssClass]">
+  <component :is="$props.component" :class="[ns.b(), csStyle]">
     <slot />
   </component>
 </template>
