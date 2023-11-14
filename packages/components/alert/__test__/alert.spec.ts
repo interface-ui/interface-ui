8e7481ffc95d012ff67a423d5ba7620deae777ae
ShihaoXiong <shihao.xiong@icloud.com>
2023-11-14 15:40:47 -0500

    test: fix test errors

--- packages/components/alert/__test__/alert.spec.ts
+++ packages/components/alert/__test__/alert.spec.ts
@@ -1,10 +1,17 @@
 import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
+import createTheme from '@fusion-ui-vue/theme'
 import { FnAlert } from '../index'
 
 describe('FnAlert', () => {
   test('class', () => {
-    const wrapper = mount(FnAlert as any)
+    const wrapper = mount(FnAlert as any, {
+      global: {
+        provide: {
+          ThemeContext: createTheme(),
+        },
+      },
+    })
     expect(wrapper.classes()).toContain('fn-alert')
   })
 })
