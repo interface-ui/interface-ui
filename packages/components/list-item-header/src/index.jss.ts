d0b7bd65b81f0869590e84b246390f84753040aa
ShihaoXiong <shihao.xiong@icloud.com>
2023-11-21 15:15:42 -0500

    feat: add intend to list item header

--- packages/components/list-item-header/src/index.jss.ts
+++ packages/components/list-item-header/src/index.jss.ts
@@ -5,8 +5,11 @@ import type { ListItemHeaderProps } from './list-item-header'
 
 const useCss: ComponentStylingHook<ListItemHeaderProps> = props =>
   computed(() => {
+    const listItemHeaderStyle = css`
+      padding: 12px 16px 12px ${16 + +props.indent * 24}px;
+    `
     const styleFromCs = props.cs ? css(props.cs) : ''
-    return cx(styleFromCs)
+    return cx(listItemHeaderStyle, styleFromCs)
   })
 
 export default useCss
--- packages/components/list-item-header/src/list-item-header.ts
+++ packages/components/list-item-header/src/list-item-header.ts
@@ -5,6 +5,10 @@ export const listItemHeaderProps = buildProps({
   cs: {
     type: [Object, String],
   },
+  indent: {
+    type: [Number, String],
+    default: 0,
+  },
 })
 
 export type ListItemHeaderProps = ExtractPropTypes<typeof listItemHeaderProps>
