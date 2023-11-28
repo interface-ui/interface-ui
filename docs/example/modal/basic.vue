6b27cdbbae0e4225c4cd53a21b38ff3f56d58716
ShihaoXiong <shihao.xiong@icloud.com>
2023-11-28 16:51:37 -0500

    docs: modal

---
+++ docs/example/modal/basic.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
+<template>
+  <fn-button variant="outlined" @click="open = !open">Open modal</fn-button>
+  <fn-modal v-model="open" />
+</template>
