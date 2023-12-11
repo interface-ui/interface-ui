0607999a20e13a61b86731f6bea57be2c2893ebf
ShihaoXiong <shihao.xiong@icloud.com>
2023-12-11 15:25:39 -0500

    perf: use button instead

--- playground/src/components/Home.vue
+++ playground/src/components/Home.vue
@@ -1,25 +1,19 @@
 <script lang="ts" setup>
-import { FnChip } from '@fusion-ui-vue/components'
+import { FnButton } from '@fusion-ui-vue/components'
 import { RouterLink } from 'vue-router'
 import routes from '../routes'
 </script>
 
 <template>
   <div class="grid-container">
-    <fn-chip
+    <fn-button
       v-for="item in routes"
-      clickable
-      :cs="{
-        width: '215px',
-        height: '75px',
-        display: 'flex',
-        fontSize: '18px',
-        justifyContent: 'center',
-      }"
       :component="RouterLink"
       :to="item.path"
+      variant="text"
+      cs="text-transform: none;"
     >
       {{ item.name }}
-    </fn-chip>
+    </fn-button>
   </div>
 </template>
