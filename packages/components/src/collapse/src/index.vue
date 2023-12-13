c3e817a8035c46643b4fc1dff7a5419b79244314
ShihaoXiong <shihao.xiong@icloud.com>
2023-12-13 16:04:23 -0500

    feat: add orientation prop to collapse (needs to be optimized)

--- packages/components/src/collapse/src/index.vue
+++ packages/components/src/collapse/src/index.vue
@@ -1,46 +1,86 @@
 <script lang="ts" setup>
 import { useNamespace } from '@fusion-ui-vue/utils'
 import { type RendererElement } from 'vue'
+import { collapseProps } from './collapse'
 
+const props = defineProps(collapseProps)
 const ns = useNamespace('collapse')
 
-const dataset = {
-  oldPaddingTop: '',
-  oldPaddingBottom: '',
-  oldOverflow: '',
-}
+const dataset =
+  props.orientation === 'vertical'
+    ? {
+        oldPaddingTop: '',
+        oldPaddingBottom: '',
+        oldOverflow: '',
+      }
+    : {
+        oldPaddingLeft: '',
+        oldPaddingRight: '',
+        oldOverflow: '',
+      }
 
 const reset = (el: RendererElement) => {
-  el.style.maxHeight = ''
-  el.style.overflow = dataset.oldOverflow
-  el.style.paddingTop = dataset.oldPaddingTop
-  el.style.paddingBottom = dataset.oldPaddingBottom
+  if (props.orientation === 'vertical') {
+    el.style.maxHeight = ''
+    el.style.overflow = dataset.oldOverflow
+    el.style.paddingTop = dataset.oldPaddingTop
+    el.style.paddingBottom = dataset.oldPaddingBottom
+  } else {
+    el.style.maxWidth = ''
+    el.style.overflow = dataset.oldOverflow
+    el.style.paddingLeft = dataset.oldPaddingLeft
+    el.style.paddingRight = dataset.oldPaddingRight
+  }
 }
 
 const listener = {
   beforeEnter(el: RendererElement) {
-    dataset.oldPaddingTop = el.style.paddingTop
-    dataset.oldPaddingBottom = el.style.paddingBottom
+    if (props.orientation === 'vertical') {
+      dataset.oldPaddingTop = el.style.paddingTop
+      dataset.oldPaddingBottom = el.style.paddingBottom
 
-    el.style.maxHeight = 0
-    el.style.paddingTop = 0
-    el.style.paddingBottom = 0
+      el.style.maxHeight = 0
+      el.style.paddingTop = 0
+      el.style.paddingBottom = 0
+    } else {
+      dataset.oldPaddingLeft = el.style.paddingLeft
+      dataset.oldPaddingRight = el.style.paddingRight
+
+      el.style.maxWidth = 0
+      el.style.paddingLeft = 0
+      el.style.paddingRight = 0
+    }
   },
 
   enter(el: RendererElement) {
     dataset.oldOverflow = el.style.overflow
-    if (el.scrollHeight !== 0) {
-      el.style.maxHeight = `${el.scrollHeight}px`
+    if (props.orientation === 'vertical') {
+      if (el.scrollHeight !== 0) {
+        el.style.maxHeight = `${el.scrollHeight}px`
+      } else {
+        el.style.maxHeight = 0
+      }
+      el.style.paddingTop = dataset.oldPaddingTop
+      el.style.paddingBottom = dataset.oldPaddingBottom
     } else {
-      el.style.maxHeight = 0
+      if (el.scrollWidth !== 0) {
+        el.style.maxWidth = `${el.scrollWidth}px`
+      } else {
+        el.style.maxWidth = 0
+      }
+      el.style.paddingLeft = dataset.oldPaddingLeft
+      el.style.paddingRight = dataset.oldPaddingRight
     }
-    el.style.paddingTop = dataset.oldPaddingTop
-    el.style.paddingBottom = dataset.oldPaddingBottom
   },
 
   afterEnter(el: RendererElement) {
-    el.style.maxHeight = ''
-    el.style.overflow = dataset.oldOverflow
+    if (props.orientation === 'vertical') {
+      el.style.maxHeight = ''
+      el.style.overflow = dataset.oldOverflow
+    } else {
+      el.style.maxWidth = ''
+      el.style.overflow = dataset.oldOverflow
+    }
   },
 
   enterCancelled(el: RendererElement) {
@@ -48,18 +88,34 @@ const listener = {
   },
 
   beforeLeave(el: RendererElement) {
-    dataset.oldPaddingTop = el.style.paddingTop
-    dataset.oldPaddingBottom = el.style.paddingBottom
-    dataset.oldOverflow = el.style.overflow
+    if (props.orientation === 'vertical') {
+      dataset.oldPaddingTop = el.style.paddingTop
+      dataset.oldPaddingBottom = el.style.paddingBottom
+      dataset.oldOverflow = el.style.overflow
 
-    el.style.maxHeight = `${el.scrollHeight}px`
+      el.style.maxHeight = `${el.scrollHeight}px`
+    } else {
+      dataset.oldPaddingLeft = el.style.paddingLeft
+      dataset.oldPaddingRight = el.style.paddingRight
+      dataset.oldOverflow = el.style.overflow
+
+      el.style.maxWidth = `${el.scrollWidth}px`
+    }
   },
 
   leave(el: RendererElement) {
-    if (el.scrollHeight !== 0) {
-      el.style.maxHeight = 0
-      el.style.paddingTop = 0
-      el.style.paddingBottom = 0
+    if (props.orientation === 'vertical') {
+      if (el.scrollHeight !== 0) {
+        el.style.maxHeight = 0
+        el.style.paddingTop = 0
+        el.style.paddingBottom = 0
+      }
+    } else {
+      if (el.scrollWidth !== 0) {
+        el.style.maxWidth = 0
+        el.style.paddingLeft = 0
+        el.style.paddingRight = 0
+      }
     }
   },
 
--- playground/src/components/Progress.vue
+++ playground/src/components/Progress.vue
@@ -8,17 +8,52 @@ const timer = setInterval(() => {
   progress.value = progress.value >= 100 ? 0 : progress.value + 10
 }, 800)
 
+// const isLoading = ref(false)
+// const onClick = () => {
+//   isLoading.value = true
+//   setTimeout(() => {
+//     isLoading.value = false
+//   }, 2000)
+// }
+
 onUnmounted(() => {
   clearInterval(timer)
 })
 </script>
 
 <template>
-  <div class="content">
-    <fn-progress status="determinate" :value="progress" />
-    <fn-progress status="indeterminate" />
-    <fn-progress status="indeterminate" size="30" />
-  </div>
+  <!-- Circle -->
+  <fn-progress status="determinate" :value="progress" />
+  <fn-progress status="indeterminate" />
+  <fn-progress status="indeterminate" size="30" />
+
+  <!-- Linear -->
   <fn-progress variant="linear" status="determinate" :value="progress" />
   <fn-progress variant="linear" status="indeterminate" />
+
+  <!-- Color -->
+  <fn-progress color="error" status="indeterminate" />
+  <fn-progress
+    :color="theme => theme.colors.cyan[400]"
+    status="indeterminate"
+  />
+  <fn-progress color="error" variant="linear" status="indeterminate" />
+  <fn-progress
+    :color="theme => theme.colors.cyan[400]"
+    variant="linear"
+    status="indeterminate"
+  />
+
+  <!-- Interactive -->
+  <!-- BUG: FnCollapse needs to be optimized -->
+  <!-- <fn-button @click="onClick" :disabled="isLoading">
+    <fn-collapse>
+      <fn-progress
+        v-if="isLoading"
+        size="32"
+        :color="isLoading ? 'primary' : 'inversePrimary'"
+      />
+    </fn-collapse>
+    Accept
+  </fn-button> -->
 </template>
