c7abc0375de6fe0d093049849bbd9284f7e56831
ShihaoXiong <shihao.xiong@icloud.com>
2024-01-10 15:30:14 -0500

    style: drawer

--- packages/core/src/drawer/src/index.jss.ts
+++ packages/core/src/drawer/src/index.jss.ts
@@ -1,17 +1,25 @@
 import { computed } from 'vue'
-import { css, cx, useColor } from '@interface-ui/theme'
+import { css, cx, useDynamicColor, useTheme } from '@interface-ui/theme'
 import type { ComponentStylingHook } from '@interface-ui/utils'
 import type { DrawerProps } from './drawer'
 
-const useCss: ComponentStylingHook<DrawerProps> = props =>
-  computed(() => {
-    const [$color] = useColor(props, 'background')
-    const drawerStyle = css`
-      --in-drawer-color: ${$color.value};
-    `
+const useCss: ComponentStylingHook<DrawerProps> = (props, ns) => {
+  const theme = useTheme()
 
-    const styleFromCs = props.cs ? css(props.cs) : ''
-    return cx(drawerStyle, styleFromCs)
+  const drawerTokens = computed(() => {
+    const { schemes } = useDynamicColor(props.background, theme)
+
+    return css(
+      schemes &&
+        ns!.cssVarBlock({
+          primary: schemes.primary,
+        }),
+    )
   })
+
+  const styleFromCs = computed(() => (props.cs ? css(props.cs) : ''))
+
+  return computed(() => cx(drawerTokens.value, styleFromCs.value))
+}
 
 export default useCss
