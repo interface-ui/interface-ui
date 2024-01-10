31cc71c45639e51b67430dbccbab05ced967a9da
ShihaoXiong <shihao.xiong@icloud.com>
2024-01-10 16:14:34 -0500

    style: input-base

--- packages/core/src/input-base/src/index.jss.ts
+++ packages/core/src/input-base/src/index.jss.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/indent */
 import { computed } from 'vue'
 import { css, cx } from '@interface-ui/theme'
 import type { ComponentStylingHook } from '@interface-ui/utils'
@@ -7,24 +6,22 @@ import type { InputBaseProps } from './input-base'
 const useCss: ComponentStylingHook<InputBaseProps> = props => {
   const styleFromCs = computed(() => (props.cs ? css(props.cs) : ''))
 
-  return computed(() => {
-    const internalStyle = props.internal
-      ? css({
-          cursor: 'inherit',
-          position: 'absolute',
-          opacity: 0,
-          width: '100%',
-          height: '100%',
-          top: '0px',
-          left: '0px',
-          margin: '0px',
-          padding: '0px',
-          zIndex: 1,
-        })
-      : ''
-
-    return cx({ [internalStyle]: props.internal }, styleFromCs.value)
+  const internalStyle = css({
+    cursor: 'inherit',
+    position: 'absolute',
+    opacity: 0,
+    width: '100%',
+    height: '100%',
+    top: '0px',
+    left: '0px',
+    margin: '0px',
+    padding: '0px',
+    zIndex: 1,
   })
+
+  return computed(() =>
+    cx({ [internalStyle]: props.internal }, styleFromCs.value),
+  )
 }
 
 export default useCss
