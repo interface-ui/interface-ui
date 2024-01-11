bc9994a98c4b03c49349f9f0b394d2e8c8b722c6
ShihaoXiong <shihao.xiong@icloud.com>
2024-01-11 15:40:11 -0500

    perf: theme-provider

--- docs/example/theme/nested.vue
+++ docs/example/theme/nested.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { ThemeProvider, createTheme } from '@interface-ui/theme'
+import { ThemeProvider, createTheme, useTheme } from '@interface-ui/theme'
 import { InCheckbox } from '@interface-ui/core'
 const checked = ref(true)
-const theme1 = createTheme('#2196F3', { target: 'host' })
-const theme2 = createTheme('#4CAF50', { target: 'host' })
+const theme = useTheme()
+const theme1 = createTheme('#2196F3', {
+  target: 'host',
+  config: { mode: theme.value.mode },
+})
+const theme2 = createTheme('#4CAF50', {
+  target: 'host',
+  config: { mode: theme.value.mode },
+})
 </script>
 
 <template>
